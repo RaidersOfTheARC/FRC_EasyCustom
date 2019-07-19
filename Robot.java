3a47629e99da4dad6c17bdd887a5bc29f09e976a
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-19 11:51:56 -0400

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,1 +1,83 @@
+/*----------------------------------------------------------------------------*/
+/* 2019 TEAR-A-BYTE #6905                                                     */
+/* Code by: Jackson Isenberg, Sahan Reddy, Shreya Das                         */
+/*----------------------------------------------------------------------------*/
 
+package frc.robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.properties file in the
+ * project.
+ */
+// If you rename or move this class, update the build.properties file in the project root
+public class Robot extends IterativeRobot {
+	
+	private static final String DEFAULT_AUTO = "Default";
+	private static final String CUSTOM_AUTO = "Sandstorm";
+	private static final long AUTO_DURATION = 7000;
+	private SendableChooser<String> chooser = new SendableChooser<>();
+	
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+
+		// send auto chooser to the radio/management
+		chooser.addDefault("Do Nothing", DEFAULT_AUTO);
+		chooser.addObject("Run Auto", CUSTOM_AUTO);
+		SmartDashboard.putData("Auto Choices", chooser);
+    
+	}
+	
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 * <p>
+	 * <p>You can add additional auto modes by adding additional comparisons to
+	 * the switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	@Override
+	public void autonomousInit() {
+    
+		// if chooser -> default
+		if (chooser.equals(DEFAULT_AUTO)) {
+			Timer.delay(AUTO_DURATION);
+		}
+		// if chooser -> custom
+		else if (chooser.equals(CUSTOM_AUTO)) {
+			autonomousPeriodic();
+		}
+    
+	}
+	
+	@Override
+	public void autonomousPeriodic() {
+    
+    // insert autonomous code here
+    
+	}
+
+	
+	/**
+	 * This function is called periodically during operator control.
+	 */
+	@Override
+	public void teleopPeriodic() {
+    
+		// insert teleop code here
+    
+	}
+}
