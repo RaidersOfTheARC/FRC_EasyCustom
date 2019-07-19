48f7219132f69087d913a8ac7b8de4bc8284a49b
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-19 13:32:18 -0400

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -64,9 +64,8 @@ public class Robot extends IterativeRobot {
 		// instantiate your motors here
 		// i.e. driveFL = new Spark(myMap.getTunerID("driveFL"));
 		
-		// default drivetrain -> differential drive
 		myDrive = new DriveTrain();
-		// customize your drivetrain above
+		// customize your drivetrain here
 	}
 	
 	/**
