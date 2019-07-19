ca03d22ba6fa624171cf103bea43ba302814b584
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-19 13:24:23 -0400

    Create DriveTrain.java

---
+++ DriveTrain.java
@@ -0,0 +1,17 @@
+/**
+ * Your drivetrain
+ * You can customize it to fit your robot's design and drive system
+ */
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.drive.*;
+
+public class DriveTrain {
+  
+  // default -> differential drive : two speed controller groups
+  public DriveTrain() {
+    
+  }
+  
+}
