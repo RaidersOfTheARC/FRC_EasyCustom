88582c07049b731008ffc5eff95ad766f4fde78d
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-19 12:15:43 -0400

    Create RobotMap.java

---
+++ RobotMap.java
@@ -0,0 +1,34 @@
+/**
+ * The robot map
+ * All of your speed controllers will be mapped here
+ * The values assigned to your speed controllers are the Tuner IDs
+ */
+
+package frc.robot;
+
+import java.util.HashMap;
+
+public class RobotMap {
+  
+  private HashMap<String, Integer> map;
+  
+  // Constructor
+  public RobotMap() {
+    
+    map = new HashMap<String, Integer>();
+    
+  }
+  
+  public void addSpeedController(String name, int id) {
+    
+    map.put(name, id);
+    
+  }
+  
+  public void getTunerID(String key) {
+  
+    map.get(key);
+  
+  }
+  
+}
