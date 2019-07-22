db3575438f389a0d52619c2af3934e30af170386
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-21 22:20:27 -0400

    Update MyRobotMap.java

--- MyRobotMap.java
+++ MyRobotMap.java
@@ -4,6 +4,11 @@ public class MyRobotMap {
   
   public RobotMap map = new RobotMap();
   
-  // 
+  public void createMap() {
+    
+    // add your devices here
+	  // i.e. myMap.addDevice("driveFL", 10); 
+    
+  }
   
 }
