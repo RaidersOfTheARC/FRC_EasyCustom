bcfa8924b28244cc00c211314d567e64830c6502
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-22 11:04:11 -0400

    Create SampleOI.java

---
+++ samples/SampleOI.java
@@ -0,0 +1,32 @@
+/**
+ * You will create your custom OI here
+ */
+
+package frc.robot.samples;
+
+import edu.wpi.first.wpilibj.*;
+import frc.robot.*;
+
+public class SampleOI {
+
+  // declare your HIDs here
+  // i.e. Joystick leftStick;
+  private Joystick leftStick, rightStick;
+  private XboxController toolOp;
+  
+  // this is your OI
+  public static OI oi;
+
+  public static void createOI() {
+    // instantiate your HIDs here
+    // i.e. leftStick = new Joystick(MyRobotMap.map.get("leftStick"));
+    leftStick = new Joystick(SampleRobotMap.map.get("leftStick"));
+    rightStick = new Joystick(SampleRobotMap.map.get("rightStick"));
+    toolOp = new XboxController(SampleRobotMap.map.get("toolOp"));
+    
+    // create your OI
+    // i.e. oi = new OI(GenericHID[]{joystick, gamepad});
+    oi = new OI(GenericHID[]{leftStick, rightStick, toolOp});
+  }
+
+}
