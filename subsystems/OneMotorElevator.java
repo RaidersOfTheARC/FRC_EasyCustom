009e41c11fe6231c0b99d3e4aa0d123966a1249b
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-21 21:37:05 -0400

    Update OneMotorElevator.java

--- subsystems/OneMotorElevator.java
+++ subsystems/OneMotorElevator.java
@@ -1,3 +1,31 @@
-public class OneMotorElevator implements Elevator {
+/**
+ * A simple elevator that uses one motor
+ */
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.qpi.first.wpilibj.*;
+import frc.robot.*;
+
+public class OneMotorElevator implements Elevator extends Subsystem {
+  
+  private String motorLoc;
+  
+  // specify whether the motor is facing left or right on the robot
+  // with regards to its natural orientation
+  public OneMotorElevator(Motor mymotor, String motorOrientation) {
+    motorLoc = motorOrientation;
+  }
+  
+  public void elevatorUp() {
+    
+  }
+  
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+  }
   
 }
