e152f491e4871b9111b70395768ba5cc24afa940
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-22 10:09:55 -0400

    Create OI.java

---
+++ OI.java
@@ -0,0 +1,22 @@
+/**
+ * All control modules go here
+ */
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.*;
+import frc.robot.subsystems.*;
+
+public class OI {
+  
+  private GenericHID[] controls;
+  
+  // user inputs a custom array of controllers
+  // i.e. OI my_OI = new OI(new GenericHID[]{joystick, gamepad});
+  public OI(GenericHID[] ctrls) {
+    controls = ctrls;
+  }
+  
+  public GenericHID getUI(int index) { return controls[index]; }
+  
+}
