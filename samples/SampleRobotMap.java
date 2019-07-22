488efc644c2bd74ad5c4f31b9e0c896326087e99
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-22 11:11:19 -0400

    Update SampleRobotMap.java

--- samples/SampleRobotMap.java
+++ samples/SampleRobotMap.java
@@ -3,19 +3,23 @@ package frc.robot.samples;
 import frc.robot.*;
 
 public class SampleRobotMap {
-  
-  public static RobotMap map = new RobotMap();
-  
-  public static void createMap() {
+	
+	public static RobotMap map = new RobotMap();
+	public static void createMap() {
     
-  	// add your devices here
-	  // i.e. myMap.addDevice("driveFL", 10);
-    map.addDevice("driveLeft", 5);
-    map.addDevice("driveRight", 6);
-    map.addDevice("leftStick", 0);
-    map.addDevice("rightStick", 1);
-    map.addDevice("toolOp", 2);
-    
-  }
+  		// add your devices here
+		// i.e. myMap.addDevice("driveFL", 10);
+    		map.addDevice("driveLeft", 5);
+		map.addDevice("driveRight", 6);
+		map.addDevice("leftStick", 0);
+		map.addDevice("rightStick", 1);
+		map.addDevice("toolOp", 2);
+	  	map.addDevice("elevatorMotor", 4);
+		map.addDevice("intakeLeft", 7);
+		map.addDevice("intakeRight", 3);
+		map.addDevice("conveyorLift", 8);
+
+
+    	}
   
 }
