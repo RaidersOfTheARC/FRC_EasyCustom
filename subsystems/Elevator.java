4a239431c53c1b3217823c6bbf1186464c77fc25
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-22 10:27:56 -0400

    Update Elevator.java

--- subsystems/Elevator.java
+++ subsystems/Elevator.java
@@ -1,10 +1,10 @@
 public interface Elevator {
 
   // the elevator goes up
-  public void elevatorUp();
+  public void elevatorUp(boolean act);
   
   // the elevator goes down
-  public void elevatorDown();
+  public void elevatorDown(boolean act);
   
   // the elevator stops
   public void elevatorStop();
