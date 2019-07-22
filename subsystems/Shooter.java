d204337ab5a576fb811ea6a9d72f371570d61bb7
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-22 18:07:08 -0400

    Update Shooter.java

--- subsystems/Shooter.java
+++ subsystems/Shooter.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems;
 public interface Shooter {
 
   // activate shooter
-  public void shoot();
+  public void shoot(boolean act);
   
   // stop shooter
   public void stopShooter();
