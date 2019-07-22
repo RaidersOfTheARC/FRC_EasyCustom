d126100e8e2e943f591c0547d85375f80ad845d9
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-22 17:35:45 -0400

    Update AngledDevice.java

--- subsystems/AngledDevice.java
+++ subsystems/AngledDevice.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems;
 public interface AngledDevice {
 
   // manipulate angle
-  public void changeAngle();
+  public void changeAngle(double degrees);
   
   // reset to default positon
   public void reset();
