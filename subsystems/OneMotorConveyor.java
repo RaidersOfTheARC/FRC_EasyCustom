1fd950828d2194fbadf5426c86b37ff4f6b1f1cd
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-22 10:49:29 -0400

    Update OneMotorConveyor.java

--- subsystems/OneMotorConveyor.java
+++ subsystems/OneMotorConveyor.java
@@ -9,4 +9,38 @@ import frc.robot.*;
 
 public class OneMotorConveyor implements Conveyor {
   
+  private SpeedController motor;
+  private String motorLoc;
+  private double power;
+  
+  // please input the motor used, its orientation, and the requested power output
+  public OneMotorConveyor(SpeedController motor, String orientation, double speed) {
+    this.motor = motor;
+    motorLoc = orientation.toLowerCase();
+    power = speed;
+  }
+  
+  public void setPower(double speed) { power = speed; }
+  
+  public void runConveyor(boolean act) {
+    if (act) {
+      if (motorLoc.equals("left")) {
+        motor.set(power);
+      } else if (motorLoc.equals("right")) {
+        motor.set(-power);
+      } else {
+        stopConveyor();
+      }
+    } else {
+      stopConveyor();
+    }
+  }
+  
+  public void stopConveyor() { motor.set(0); }
+  
+  public void invertConveyor() {
+    if (!motor.getInverted()) motor.setInverted(true);
+    else motor.setInverted(false);
+  }
+  
 }
