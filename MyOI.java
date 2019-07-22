ed4454f01cfb0732fa1af6d8eb39f0a02123188e
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-22 10:22:39 -0400

    Create MyOI.java

---
+++ MyOI.java
@@ -0,0 +1,25 @@
+/**
+ * You will create your custom OI here
+ */
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.*;
+
+public class MyOI {
+
+  // declare your HIDs here
+  // i.e. Joystick leftStick;
+  
+  // this is your OI
+  public static OI oi;
+
+  public static void createOI() {
+    // instantiate your HIDs here
+    // i.e. leftStick = new Joystick(MyRobotMap.map.get("leftStick"));
+    
+    // create your OI
+    // i.e. oi = new OI(GenericHID[]{joystick, gamepad});
+  }
+
+}
