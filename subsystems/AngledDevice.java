06c1809d400d8339a6e351c104ec87d9cb7d2c3d
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-22 14:24:54 -0400

    Create AngledDevice.java

---
+++ subsystems/AngledDevice.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+public interface AngledDevice {
+
+  // manipulate angle
+  public void changeAngle();
+  
+  // reset to default positon
+  public void reset();
+
+}
