cdab3e7b864d2642caa39e1944aa6d2a2bc0d615
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-22 12:54:20 -0400

    Update OneCylinderTrigger.java

--- subsystems/OneCylinderTrigger.java
+++ subsystems/OneCylinderTrigger.java
@@ -1,1 +1,34 @@
+/**
+ * A push trigger that uses one cylinder
+ */
 
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.command.*;
+
+public class OneCylinderTrigger implements PushTrigger extends Subsystem {
+  
+  private Solenoid trigger
+  
+  public OneMotorConveyor(Solenoid trigger) {
+    this.trigger = trigger;
+  }
+    
+  public void push(boolean act) {
+    if (act) {
+      trigger.set(true);
+    } else {
+      retract();
+    }
+  }
+  
+  public void stopConveyor() { trigger.set(false); }
+  
+  @Override
+  	public void initDefaultCommand() {
+		  // Set the default command for a subsystem here.
+	  	// setDefaultCommand(new MySpecialCommand());
+  	}
+  
+}
