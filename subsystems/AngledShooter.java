eac447576cf7bc742b64cd821e1bed1e51b99afc
Jackson Isenberg <jisenberg1050@gmail.com>
2019-07-22 17:51:10 -0400

    Update AngledShooter.java

--- subsystems/AngledShooter.java
+++ subsystems/AngledShooter.java
@@ -1,7 +1,31 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.*;
+
 public abstract class AngledShooter implements AngledDevice, Shooter {
-
   
+  public Servo[] angleManips;
+  public SpeedController[] shooters;
 
+  public AngledShooter(Servo[] anglers, SpeedController[] shooters) {
+    angleManips = anglers;
+    this.shooters = shooters;
+  }
+  
+  public Servo getAngleServo(int index) {
+    return angleManips[index];
+  }
+  
+  public SpeedController getShooterMotor(int index) {
+    return shooters[index];
+  }
+  
+  public Servo[] getServos() {
+    return angleManips;
+  }
+  
+  public SpeedController[] getShooters() {
+    return shooters;
+  }
+  
 }
