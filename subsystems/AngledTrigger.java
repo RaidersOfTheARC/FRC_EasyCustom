234cf617d59affcc0ae5008899b740f64cb1f568
Jackson Isenberg <jisenberg1050@gmail.com>
2019-08-05 13:35:59 -0400

    Delete AngledTrigger.java

--- subsystems/AngledTrigger.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems;
-
-import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.command.*;
-
-public abstract class AngledTrigger extends Subsystem implements AngledDevice, PushTrigger {
-  
-}
