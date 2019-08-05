ffe76b811e7b564ce2b63f935e446584992ca3a3
Jackson Isenberg <jisenberg1050@gmail.com>
2019-08-05 13:17:32 -0400

    Update AngledTrigger.java

--- subsystems/AngledTrigger.java
+++ subsystems/AngledTrigger.java
@@ -1,1 +1,8 @@
+package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.command.*;
+
+public abstract class AngledTrigger extends Subsystem implements AngledDevice, PushTrigger {
+  
+}
